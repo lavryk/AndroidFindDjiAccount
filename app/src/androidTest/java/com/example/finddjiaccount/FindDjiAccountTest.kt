c7185af918ba1666aa5f20a977f333157142f90d
ylavr <ylavryk@gmail.com>
2025-12-08 14:03:43 +0200

    Refactoring

--- app/src/androidTest/java/com/example/finddjiaccount/FindDjiAccountTest.kt
+++ app/src/androidTest/java/com/example/finddjiaccount/FindDjiAccountTest.kt
@@ -3,6 +3,7 @@ package com.example.finddjiaccount
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.SystemClock
+import androidx.annotation.NonNull
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.By
@@ -29,37 +30,38 @@ import java.nio.file.Paths
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 
-const val SERIAL : String = "1581F7FVC258700DK18X"
-const val EMAIL_PREFIX : String = "d24"
+const val SERIAL : String = "1581F7K3C252D00C44EP"
+const val EMAIL_PREFIX : String = "c87"
 const val EMAIL_SUFFIX : String = "@djifly.pl"
-const val PASS_PREFIX : String = "DJIfly24"
+const val PASS_PREFIX : String = "DJIfly87"
 const val START_COUNT : Int = 0
 const val CYCLE_COUNT : Int = 100
-const val GEMINI_API_KEY = "--------"
+const val GEMINI_API_KEY = "AIzaSyB6me-V_dwqp8xq96oYkQBIzxAJVvOBnjI"
+private var uiDevice: UiDevice? = null
 
 @RunWith(AndroidJUnit4::class)
 class FindAccountTest {
 
-    fun findElement(sn: String, items: List<UiObject2>, device: UiDevice) : Boolean {
+    fun findSerialElement(items: List<UiObject2>) : Boolean {
         for (item in items) {
             item.click()
             SystemClock.sleep(100L)
-            if (device.findObject(By.text(sn)) != null) {
+            if (getDevice().findObject(By.text(SERIAL)) != null) {
                 return true
             }
         }
         return false
     }
-    fun findMatrice(sn: String, device: UiDevice) : Boolean {
-        val panelObj = device.findObject(By.clazz("androidx.recyclerview.widget.RecyclerView"))
+    fun findMatrice() : Boolean {
+        val panelObj = getDevice().findObject(By.clazz("androidx.recyclerview.widget.RecyclerView"))
         do {
-            val items = device.findObjects(By.text("Matrice 4 Series"))
+            val items = getDevice().findObjects(By.text("Matrice 4 Series"))
 
             if(items.count() < 2) {
                 break
             }
 
-            if(findElement(sn, items, device)) {
+            if(findSerialElement( items)) {
                 return true
             }
 
@@ -67,19 +69,29 @@ class FindAccountTest {
 
         SystemClock.sleep(100L)
 
-        return findElement(sn, device.findObjects(By.text("Matrice 4 Series")), device)
+        return findSerialElement(getDevice().findObjects(By.text("Matrice 4 Series")))
     }
 
-    fun isCaptchaExists(device: UiDevice) : Boolean {
-        return device.findObject(By.res("com.dji.industry.pilot:id/sign_in_verify_code_btn")) != null
+    companion object {
+        @JvmStatic // Use @JvmStatic for Java interoperability if needed
+        @NonNull
+        fun getDevice(): UiDevice {
+            if (uiDevice == null) {
+                uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+            }
+            return uiDevice!!
+        }
+    }
+
+    fun isCaptchaExists() : Boolean {
+        return getDevice().findObject(By.res("com.dji.industry.pilot:id/sign_in_verify_code_btn")) != null
     }
     fun resolveCaptcha() : String? {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         val dir = context.filesDir
-        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-        val img = device.findObject(By.res("com.dji.industry.pilot:id/sign_in_verify_code_btn"))
+        val img = getDevice().findObject(By.res("com.dji.industry.pilot:id/sign_in_verify_code_btn"))
         val rect = img.visibleBounds
-        val result: Boolean = device.takeScreenshot(File("${dir}/screenshot.png"))
+        val result: Boolean = getDevice().takeScreenshot(File("${dir}/screenshot.png"))
         if (result) {
             val cropped = Bitmap.createBitmap(
                 BitmapFactory.decodeFile(File("${dir}/screenshot.png").absolutePath),
@@ -115,67 +127,67 @@ class FindAccountTest {
         }
     }
 
-    fun findInput(device: UiDevice, id: String) : UiObject {
-        return device.findObject(UiSelector().resourceId(id))
+    fun findInput(id: String) : UiObject {
+        return getDevice().findObject(UiSelector().resourceId(id))
     }
 
-    fun emailInput(device : UiDevice) : UiObject {
-        return findInput(device, "com.dji.industry.pilot:id/sign_in_account")
+    fun emailInput() : UiObject {
+        return findInput("com.dji.industry.pilot:id/sign_in_account")
     }
 
-    fun passwordInput(device: UiDevice) : UiObject {
-        return findInput(device, "com.dji.industry.pilot:id/sign_in_password")
+    fun passwordInput() : UiObject {
+        return findInput("com.dji.industry.pilot:id/sign_in_password")
     }
 
-    fun logOut(device: UiDevice) {
-        findInput(device,"com.dji.industry.pilot:id/home_logout_btn").click()
-        device.wait(
+    fun logOut() {
+        findInput("com.dji.industry.pilot:id/home_logout_btn").click()
+        getDevice().wait(
             Until.findObject(By.text("OK")),
             2000   // timeout ms
         )?.click()
     }
 
-    fun logIn(device: UiDevice, username: String, password: String) : Boolean {
-        emailInput(device).setText(username)
-        passwordInput(device).setText(password)
+    fun logIn(username: String, password: String) : Boolean {
+        emailInput().setText(username)
+        passwordInput().setText(password)
 
-        if(isCaptchaExists(device)) {
-            findInput(device, "com.dji.industry.pilot:id/sign_in_verify_code_edit").setText(resolveCaptcha())
+        if(isCaptchaExists()) {
+            findInput( "com.dji.industry.pilot:id/sign_in_verify_code_edit").setText(resolveCaptcha())
         }
 
-        findInput(device, "com.dji.industry.pilot:id/toolbar_log_in").click()
-        device.wait(
+        findInput("com.dji.industry.pilot:id/toolbar_log_in").click()
+        getDevice().wait(
             Until.findObject(By.text("OK")),
             2000
         )?.click()
 
         SystemClock.sleep(100L)
 
-        return !passwordInput(device).exists() && !emailInput(device).exists()
+        return !passwordInput().exists() && !emailInput().exists()
     }
 
-    fun logoutLogin(device : UiDevice, username: String, password: String) : Boolean {
-        val enterObj = device.findObject(By.text("Вход"))
+    fun logoutLogin(username: String, password: String) : Boolean {
+        val enterObj = getDevice().findObject(By.text("Вход"))
         if(enterObj != null) {
             enterObj.click()
         } else {
-            logOut(device)
-            device.wait(
+            logOut()
+            getDevice().wait(
                 Until.findObject(By.text("Вход")),
                 2000
             )?.click()
         }
         SystemClock.sleep(200L)
         repeat(3) {
-            if(logIn(device, username, password)) {
+            if(logIn( username, password)) {
                 return true
             }
         }
         SystemClock.sleep(200L)
         return false
     }
-    fun goBack(device: UiDevice) {
-        findInput(device, "com.dji.industry.pilot:id/navStartContainer").click()
+    fun goBack() {
+        findInput( "com.dji.industry.pilot:id/navStartContainer").click()
     }
 
     @Test
@@ -193,13 +205,13 @@ class FindAccountTest {
                 if(i < 10) {
                     number = "0$number"
                 }
-                if(logoutLogin(device, "$EMAIL_PREFIX$number$EMAIL_SUFFIX", "$PASS_PREFIX$number")) {
-                    findInput(device,"com.dji.industry.pilot:id/item_title").click()
-                    if(findMatrice(SERIAL, device)) {
+                if(logoutLogin("$EMAIL_PREFIX$number$EMAIL_SUFFIX", "$PASS_PREFIX$number")) {
+                    findInput("com.dji.industry.pilot:id/item_title").click()
+                    if(findMatrice()) {
                         println("#### FOUND")
                         break
                     } else {
-                        goBack(device)
+                        goBack()
                     }
                 }
             }
